679ac80ab659f046d7ce0ec9ab791950c6e0d135
fsubal <fsubal1102@gmail.com>
2022-03-09 00:20:15 +0900

    chore(icons): use styled-components

--- packages/icons/src/PixivIcon.story.tsx
+++ packages/icons/src/PixivIcon.story.tsx
@@ -1,7 +1,7 @@
 /// <reference types='@types/webpack-env' />
 
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import TestIconThatNeverExists from './16/TestIconThatNeverExists.svg'
 import { Props } from './PixivIcon'
 import { PixivIcon } from '.'
@@ -55,42 +55,74 @@ const groupedIcons = KNOWN_ICON_FILES.reduce<Record<string, KnownIconFile[]>>(
   {}
 )
 
+const Global = createGlobalStyle`
+  :root {
+    font-family: Helvetica, Arial, sans-serif;
+    color: rgba(#000, 0.88);
+  }
+
+  .icon-class {
+    transform: rotate(45deg);
+  }
+`
+
+const Grid = styled.div`
+  display: grid;
+  gap: 8px;
+  grid-template-columns: 1fr 1fr 1fr;
+`
+
+const Group = styled.div`
+  & + & {
+    margin-top: 64px;
+  }
+`
+
+const IconDef = styled.div<{ color: string }>`
+  color: ${({ color }) => color};
+  display: inline-flex;
+  align-items: center;
+  min-height: 32px;
+
+  pixiv-icon {
+    display: block;
+    flex-shrink: 0;
+
+    & + div {
+      flex: 1 0;
+      margin-left: 8px;
+      font-size: 14px;
+      line-height: 22px;
+    }
+  }
+`
+
+const Heading = styled.h2`
+  font-size: 20px;
+  line-height: 28px;
+  font-weight: bold;
+  margin: 16px 0;
+`
+
 const DefaultStory: Story<{
   scale: NonNullable<Props['scale']>
   color: string
 }> = ({ scale, color }) => (
   <>
     {Object.entries(groupedIcons).map(([groupName, icons]) => (
-      <div key={groupName}>
-        <h2>
+      <Group key={groupName}>
+        <Heading>
           {groupName} (scale: {scale})
-        </h2>
-        <div
-          style={{
-            color,
-            display: 'grid',
-            gap: 8,
-            gridTemplateColumns: '1fr 1fr 1fr',
-          }}
-        >
+        </Heading>
+        <Grid>
           {icons.map((name) => (
-            <div
-              key={name}
-              style={{
-                display: 'inline-flex',
-                alignItems: 'center',
-                gap: 8,
-                minHeight: 32,
-              }}
-            >
-              <div style={{ flexShrink: 0 }}>
-                <pixiv-icon key={scale} name={name} scale={scale} />
-              </div>
-              <div style={{ flex: '1 0' }}>{name}</div>
-            </div>
+            <IconDef color={color} key={name}>
+              <pixiv-icon key={scale} name={name} scale={scale} />
+              <div>{name}</div>
+            </IconDef>
           ))}
-        </div>
-      </div>
+        </Grid>
+      </Group>
     ))}
     <Global />
   </>
@@ -100,22 +132,16 @@ export const Default = DefaultStory.bind({})
 
 Default.args = { scale: 1, color: '#000000' }
 
-const Global = createGlobalStyle`
-  .icon-class {
-    transform: rotate(45deg);
-  }
-`
-
 export const WithAttributes: Story<Props & { color: string }> = ({
   name,
   scale,
   color,
 }) => (
   <>
-    <div style={{ color, display: 'inline-flex', alignItems: 'center' }}>
+    <IconDef color={color}>
       <pixiv-icon class="icon-class" name={name} scale={scale} />
-      アイコンと文字
-    </div>
+      <div>アイコンと文字</div>
+    </IconDef>
     <Global />
   </>
 )
@@ -128,10 +154,10 @@ export const WithUnsafe: Story<Props & { color: string }> = ({
   color,
 }) => (
   <>
-    <div style={{ color, display: 'inline-flex', alignItems: 'center' }}>
+    <IconDef color={color}>
       <pixiv-icon unsafe-non-guideline-scale="3.75" name={name} scale={scale} />
       アイコンと文字
-    </div>
+    </IconDef>
     <Global />
   </>
 )
