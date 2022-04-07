911c87db773d2a58cfd1d09a3b1f13f8b279139d
pnly <pnly@pixiv.com>
2022-04-07 18:17:06 +0900

    chore: use globalThis for testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,11 @@
-// eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
-declare var ResizeObserver: any
+export {}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-ResizeObserver = jest.fn().mockImplementation(() => ({
+declare global {
+  // eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
+  var ResizeObserver: any
+}
+
+globalThis.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
   unobserve: jest.fn(),
   disconnect: jest.fn(),
