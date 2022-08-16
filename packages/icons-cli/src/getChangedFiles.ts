0d76672b2d4cc02e56caaaa78d1fa36e6361af8e
mimokmt <mimo@pixiv.co.jp>
2022-08-16 10:00:04 +0900

    fix: icons cli

--- packages/icons-cli/src/getChangedFiles.ts
+++ packages/icons-cli/src/getChangedFiles.ts
@@ -8,37 +8,15 @@ export const targetDir = path.resolve(process.cwd(), 'packages', 'icons')
  * dir 内で変更があったファイル情報を for await で回せるようにするやつ
  */
 export async function* getChangedFiles(dir = targetDir) {
-  const directories = await fs.readdir(dir)
   const gitStatus = await collectGitStatus()
-
-  for (const dir of directories) {
-    const directory = path.resolve(targetDir, dir)
-
-    // eslint-disable-next-line no-await-in-loop
-    const stat = await fs.stat(directory)
-    if (!stat.isDirectory()) {
+  const map = new Map(Object.entries(gitStatus))
+  for (const [relativePath, status] of map) {
+    const fullpath = path.resolve(process.cwd(), relativePath)
+    if (!`${fullpath}`.startsWith(`${dir}/`)) {
       continue
     }
-
-    // eslint-disable-next-line no-await-in-loop
-    const files = await fs.readdir(directory)
-
-    for (const file of files) {
-      const fullpath = path.resolve(targetDir, dir, file)
-      const relativePath = path.relative(process.cwd(), fullpath)
-
-      const status = gitStatus[relativePath]
-      console.log(`${fullpath}: ${status ?? 'null'}`)
-      if (status == null) {
-        // Already up-to-date
-        continue
-      }
-
-      // eslint-disable-next-line no-await-in-loop
-      const content = await fs.readFile(fullpath, { encoding: 'utf-8' })
-
-      yield { relativePath, content, status }
-    }
+    const content = await fs.readFile(fullpath, { encoding: 'utf-8' })
+    yield { relativePath, content, status }
   }
 }
 
@@ -47,20 +25,17 @@ async function collectGitStatus() {
     /**
      * @see https://git-scm.com/docs/git-status#_porcelain_format_version_1
      */
-    (await execp(`git status --porcelain`))
-      .split('\n')
-      .map(
-        (s) =>
-          [
-            s.slice(3),
-            s.startsWith(' M')
-              ? 'modified'
-              : s.startsWith('??')
-              ? 'untracked'
-              : s.startsWith(' D')
-              ? 'deleted'
-              : null,
-          ] as const
-      )
+    (await execp(`git status --porcelain`)).split('\n').map((s) => {
+      return [
+        s.slice(3),
+        s.startsWith(' M')
+          ? 'modified'
+          : s.startsWith('??')
+          ? 'untracked'
+          : s.startsWith(' D')
+          ? 'deleted'
+          : null,
+      ] as const
+    })
   )
 }
