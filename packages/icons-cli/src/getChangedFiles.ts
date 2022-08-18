6e1425e4098900a4da821ea5c4e2a7ea2ccf4d04
mimokmt <mimo@pixiv.co.jp>
2022-08-18 19:24:24 +0900

    chore: check files and directories exists

--- packages/icons-cli/src/getChangedFiles.ts
+++ packages/icons-cli/src/getChangedFiles.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs'
+import { promises as fs, existsSync } from 'fs'
 import path from 'path'
 import { execp } from './utils'
 
@@ -8,13 +8,17 @@ export const targetDir = path.resolve(process.cwd(), 'packages', 'icons')
  * dir 内で変更があったファイル情報を for await で回せるようにするやつ
  */
 export async function* getChangedFiles(dir = targetDir) {
+  if (!existsSync(dir))
+    throw new Error(`icons-cli: target directory not found (${dir})`)
   const gitStatus = await collectGitStatus()
   const map = new Map(Object.entries(gitStatus))
   for (const [relativePath, status] of map) {
     const fullpath = path.resolve(process.cwd(), relativePath)
-    if (!`${fullpath}`.startsWith(`${dir}/`)) {
+    if (!fullpath.startsWith(`${dir}/`)) {
       continue
     }
+    if (!existsSync(fullpath))
+      throw new Error(`icons-cli: could not load svg (${fullpath})`)
     const content = await fs.readFile(fullpath, { encoding: 'utf-8' })
     yield { relativePath, content, status }
   }
