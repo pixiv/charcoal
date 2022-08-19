7180d4dac09a8c4acdc2470043a55f4fa105dbfb
abcang <abcang1015@gmail.com>
2022-08-19 10:49:09 +0900

    fix: fix jest

---
+++ misc/fileTransformer.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  process(sourceText, sourcePath, options) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
+  },
+};
