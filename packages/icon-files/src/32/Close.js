291b8b7d7f48f6ef586161e520d17c89df5ec403
mimokmt <mimo@pixiv.co.jp>
2022-10-27 17:38:42 +0900

    chore: icon-filesの追加

---
+++ packages/icon-files/src/32/Close.js
@@ -0,0 +1,2 @@
+/** This file is auto generated. DO NOT EDIT BY HAND. */
+export default '<svg width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M18.828 16l4.586 4.586a2 2 0 11-2.828 2.828L16 18.828l-4.586 4.586a2 2 0 11-2.828-2.828L13.172 16l-4.586-4.586a2 2 0 112.828-2.828L16 13.172l4.586-4.586a2 2 0 112.828 2.828L18.828 16z" fill="currentColor"></path></svg>'
