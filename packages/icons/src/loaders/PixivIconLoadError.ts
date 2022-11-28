07cb145269b46d1c5d22fb64857d4a2c4a9e8ef1
fsubal <subal@pixiv.co.jp>
2022-11-28 17:58:07 +0900

    chore: add formatMessage

--- packages/icons/src/loaders/PixivIconLoadError.ts
+++ packages/icons/src/loaders/PixivIconLoadError.ts
@@ -1,13 +1,21 @@
 export class PixivIconLoadError extends Error {
   constructor(name: string, cause: unknown) {
-    let message = `Failed to fetch <pixiv-icon name="${name}">`
-    if (cause instanceof Error) {
-      message += `: ${cause.toString()})`
-    } else if (cause != null) {
-      message += `: ${JSON.stringify(cause)})`
-    }
+    const message = formatMessage(name, cause)
 
     super(message, { cause })
     Object.setPrototypeOf(this, new.target)
   }
+}
+
+function formatMessage(name: string, cause: unknown) {
+  const message = `Failed to fetch <pixiv-icon name="${name}">`
+  if (cause == null) {
+    return message
+  }
+
+  if (cause instanceof Error) {
+    return `${message}: ${cause.toString()})`
+  }
+
+  return `${message}: ${JSON.stringify(cause)})`
 }
