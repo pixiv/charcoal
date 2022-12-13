e2807828c1739c84df8a83442d07865a0f74a9c2
mimokmt <mimo@pixiv.co.jp>
2022-12-13 11:58:41 +0900

    chore: run icons-cli files:generate

--- packages/icon-files/src/24/Feed.js
+++ packages/icon-files/src/24/Feed.js
@@ -1,2 +1,2 @@
 /** This file is auto generated. DO NOT EDIT BY HAND. */
-export default '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.5 6a2.5 2.5 0 000 5h9a2.5 2.5 0 000-5h-9zM9.5 13a2.5 2.5 0 000 5h9a2.5 2.5 0 000-5h-9z" fill="currentColor"></path></svg>'
+export default '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 6a2.5 2.5 0 000 5h9a2.5 2.5 0 000-5h-9zM9.5 13a2.5 2.5 0 000 5h9a2.5 2.5 0 000-5h-9z" fill="currentColor"/></svg>'
