c99b475f998fa9acde871e43bbf55b5a68b603f8
toshusai <shu@pixiv.co.jp>
2023-02-01 14:44:10 +0900

    docs: add comment

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -6,10 +6,13 @@ export default defineConfig({
   format: ['esm', 'cjs'],
   target: 'esnext',
   sourcemap: true,
+  // Next.jsでcjsを使うときにエラーになるので回避策としてstyled-componentsのrequireを書き換える
+  // TypeError: import_styled_components2.default.button is not a function
   onSuccess: () => {
     const cjsDistFile = './dist/index.cjs'
     let src = readFileSync(cjsDistFile).toString()
     src = src.replace(
+      // NOTE: この正規表現は出力結果を見て作成した
       /__toESM\(require\("styled-components"\), 1\);/g,
       `require("styled-components");`
     )
