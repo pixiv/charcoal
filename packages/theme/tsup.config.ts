338069165275079baf9c54d4b7bdd964e203a026
Ciffelia <mc.prince.0203@gmail.com>
2023-03-13 12:26:22 +0900

    build: 全パッケージでmicrobundleからesbuildに移行

---
+++ packages/theme/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  outExtension({ format }) {
+    return {
+      js: `.${format}.js`,
+    }
+  },
+  target: 'esnext',
+  sourcemap: true,
+})
