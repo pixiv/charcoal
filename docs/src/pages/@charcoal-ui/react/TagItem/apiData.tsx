8ae3a445ab96ff1512630b651475c2efcd4af913
toshusai <shu@pixiv.co.jp>
2023-04-10 14:20:33 +0900

    docs: update docs

--- docs/src/pages/@charcoal-ui/react/TagItem/apiData.tsx
+++ docs/src/pages/@charcoal-ui/react/TagItem/apiData.tsx
@@ -5,19 +5,16 @@ export const apiData: Partial<ApiTableData<TagItemProps, HTMLInputElement>> = {
   bgColor: {
     default: '',
     description: '背景色、CSSで扱える文字列',
-    required: false,
     type: 'string',
   },
   bgImage: {
     default: '',
     description: '背景画像のURL',
-    required: false,
     type: 'string',
   },
   href: {
     default: '',
     description: 'a要素のhref',
-    required: false,
     type: 'string',
   },
   label: {
@@ -29,38 +26,32 @@ export const apiData: Partial<ApiTableData<TagItemProps, HTMLInputElement>> = {
   rel: {
     default: '',
     description: 'a要素のrel',
-    required: false,
     type: 'boolean',
   },
   size: {
     default: '"M"',
     description: '大きさ',
-    required: false,
     type: '"S" | "M"',
   },
   status: {
     default: '"default"',
     description: 'タグの状態',
-    required: false,
     type: '"default" | "active" | "inactive"',
   },
   target: {
     default: '',
     description: 'a要素のtarget',
-    required: false,
     type: 'boolean',
   },
   translatedLabel: {
     default: '',
     description:
       '翻訳されたタグ、labelの代わりに表示され、labelは下に表示される。',
-    required: false,
     type: 'string',
   },
   disabled: {
     default: '',
-    description: '無効かどうか',
-    required: false,
+    description: '無効化',
     type: 'boolean',
   },
 }
