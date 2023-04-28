fc8f90f674a5915ca76d8135b131ad7ff0206ec3
toshusai <shu@pixiv.co.jp>
2023-04-28 10:41:14 +0900

    refactor: remove sample

--- packages/styled/src/TokenInjector.tsx
+++ packages/styled/src/TokenInjector.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createGlobalStyle, css } from 'styled-components'
+import styled, { createGlobalStyle, css } from 'styled-components'
 import { CharcoalAbstractTheme } from '@charcoal-ui/theme'
 import { defineThemeVariables } from './util'
 
