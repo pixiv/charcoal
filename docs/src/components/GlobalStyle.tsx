50c5b85d9b270baf1b79a24b8a29f9de212dbd28
fsubal <subal@pixiv.co.jp>
2023-04-28 21:29:03 +0900

    chore: add supported scales

--- docs/src/components/GlobalStyle.tsx
+++ docs/src/components/GlobalStyle.tsx
@@ -7,18 +7,35 @@ const resetPixivIcon = css`
     width: calc(var(--icon-size, 1em) * var(--scale, 1));
     height: calc(var(--icon-size, 1em) * var(--scale, 1));
 
+    &[name^='Inline/'] {
+      --icon-size: 1em;
+
+      &[scale='2'] {
+        --icon-size: 2em;
+      }
+    }
+
     &[name^='16/'] {
       --icon-size: 16px;
     }
 
     &[name^='24/'] {
       --icon-size: 24px;
+
+      &[scale='2'] {
+        --icon-size: 48px;
+      }
+
+      &[scale='3'] {
+        --icon-size: 72px;
+      }
     }
 
     &[name^='32/'] {
       --icon-size: 32px;
     }
 
+    /** NOTICE: 現状だと attr(... number) はほとんどのブラウザで動かない */
     &[scale] {
       --scale: attr(scale number);
     }
