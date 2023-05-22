d36bf222c5c72657e17dc425f26e77d3cdca8f6d
fsubal <subal@pixiv.co.jp>
2023-05-22 19:58:41 +0900

    chore: write about bundler

--- docs/src/pages/@charcoal-ui/icons/extend.page.tsx
+++ docs/src/pages/@charcoal-ui/icons/extend.page.tsx
@@ -40,6 +40,44 @@ export default function ExtendPage() {
         TypeScript 環境下では、<InlineCode>KnownIconType</InlineCode>
         という型を拡張することで、カスタムアイコンに対しても補完が効くようになります。
       </p>
+
+      <h2>独自アイコンのためのバンドラ設定</h2>
+      <p>
+        <InlineCode>PixivIcon.extend()</InlineCode>
+        に渡すオブジェクトはキーがアイコン名、値がSVGファイルに対するパス名またはURLである必要があります。
+        <br />
+        （内部的には、ここで渡した文字列に対して<InlineCode>fetch()</InlineCode>
+        が走ります）
+      </p>
+      <p>
+        SVGファイルを<InlineCode>import</InlineCode>
+        して渡す場合、プロジェクトのバンドラが<InlineCode>*.svg</InlineCode>を
+        <strong>パス文字列として（ファイルの内容の文字列ではなく）</strong>
+        ロードする設定になっていることを確認してください。
+      </p>
+      <p>
+        たとえばWebpackの場合、当該アイコンファイルに対するルールは
+        <a
+          href="https://webpack.js.org/guides/asset-modules/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InlineCode>type: 'asset/resource'</InlineCode>
+        </a>
+        であるべきです。
+        <br />
+        <a
+          href="https://react-svgr.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          svgr
+        </a>
+        など、SVGファイルを文字列以外の値にトランスフォームするツールを使っているプロジェクトでは、
+        <br />
+        <InlineCode>PixivIcon.extend()</InlineCode>に渡す用の
+        <InlineCode>.svg</InlineCode>ファイルをその対象から除いてください。
+      </p>
     </ContentRoot>
   )
 }
