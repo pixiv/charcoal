05cb2033da5827246de8ea97902aed54076f21f5
naporin0624 <naporin0624@gmail.com>
2023-07-28 06:55:40 +0900

    feat(react): add checkbox performance test

---
+++ packages/react/src/components/Checkbox/performance.test.tsx
@@ -0,0 +1,30 @@
+import { light } from '@charcoal-ui/theme'
+import 'jest-styled-components'
+
+import React from 'react'
+import renderder from 'react-test-renderer'
+import { ThemeProvider } from 'styled-components'
+import Checkbox from '.'
+
+export function render(children: JSX.Element) {
+  return renderder
+    .create(<ThemeProvider theme={light}>{children}</ThemeProvider>)
+    .toJSON()
+}
+
+const array = [...Array<undefined>(1000)].map((_, i) => i)
+
+describe('Checkbox', () => {
+  test('<Checkbox />', () => {
+    function ManyCheckbox() {
+      return (
+        <>
+          {array.map((i) => (
+            <Checkbox key={i} label="test" />
+          ))}
+        </>
+      )
+    }
+    render(<ManyCheckbox />)
+  })
+})
