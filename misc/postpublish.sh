b1adb659df3a777f7fcf48cbd4e6a384e2d4fd27
mimokmt <mimo@pixiv.co.jp>
2023-07-27 17:06:22 +0900

    fix: not push current branch commits

--- misc/postpublish.sh
+++ misc/postpublish.sh
@@ -5,14 +5,14 @@ set -euxo pipefail
 # project root で実行
 cd $(dirname $0)/..
 
-default_branch=$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')
+current_branch=$(git symbolic-ref --short HEAD)
 
 yarn install
 
 if [[ $(git status --porcelain | grep yarn.lock) ]]; then
   git add yarn.lock
   git commit -m "chore: yarn install after publish"
-  git push origin $default_branch
+  git push origin $current_branch
 else
   echo 'No diff found after yarn install'
 fi
