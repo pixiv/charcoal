ad16c55bc8582762f963495ca343908ba6322e01
naporin0624 <naporin0624@gmail.com>
2023-09-27 16:16:54 +0900

    chore: update ci and config

--- storyshots.config.mjs
+++ storyshots.config.mjs
@@ -1,18 +1,20 @@
 import { config } from './jest.config.mjs'
 
+const defaultConfig = config()
+
 export default {
-  ...config(),
+  ...defaultConfig,
   setupFilesAfterEnv: ['./jest.setup.ts'],
   transform: {
-    ...config().transform,
+    ...defaultConfig.transform,
     '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
   },
   moduleNameMapper: {
+    ...defaultConfig.moduleNameMapper,
     '^@charcoal-ui/(.*)$': '<rootDir>/packages/$1/src',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file.ts',
     '\\.(styl|css|less|scss)$': '<rootDir>/__mocks__/style.ts',
-    '\\.(mdx)$': '<rootDir>/__mocks__/file.ts',
   },
   testMatch: ['<rootDir>/**/storyshots.test.(js|jsx|ts|tsx)'],
 }
