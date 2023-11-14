5bc620ad850ba6bd93b50d8dceb7c2e3de5fb6a0
toshusai <toshusai.net@gmail.com>
2023-11-14 12:55:43 +0900

    test: add story

--- packages/react/src/components/Popover/index.story.tsx
+++ packages/react/src/components/Popover/index.story.tsx
@@ -1,5 +1,6 @@
 import Popover from '.'
 export { Basic } from './__stories__/Basic'
+export { Overflow } from './__stories__/Overflow'
 export { Hover } from './__stories__/Hover'
 export { Dir } from './__stories__/Dir'
 
