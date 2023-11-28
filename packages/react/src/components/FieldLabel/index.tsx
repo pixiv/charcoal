31c8680b7f3fe11ba4e04c0927744b0848b08206
naporin0624 <naporin0624@gmail.com>
2023-11-29 04:36:33 +0900

    chore(react): fix lint error

--- packages/react/src/components/FieldLabel/index.tsx
+++ packages/react/src/components/FieldLabel/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { theme } from '../../styled'
 
 export interface FieldLabelProps
   extends React.LabelHTMLAttributes<HTMLLabelElement> {
