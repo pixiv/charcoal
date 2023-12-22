fca78fffb85c19f2217d88f9b6103fb7ee2c59dc
mimo <mimo@pixiv.co.jp>
2023-12-22 18:01:16 +0900

    feat(react): dropdown selector listitem updated to csf3

--- packages/react/src/components/DropdownSelector/ListItem/index.story.tsx
+++ packages/react/src/components/DropdownSelector/ListItem/index.story.tsx
@@ -1,51 +1,54 @@
 import { useState } from 'react'
-import { Story } from '../../../_lib/compat'
 import Icon from '../../Icon'
 import Switch from '../../Switch'
-import ListItem, { ListItemProps } from '.'
+import ListItem from '.'
 import styled from 'styled-components'
+import { Meta, StoryObj } from '@storybook/react'
 
 export default {
   title: 'DropdownSelector/ListItem',
   component: ListItem,
-}
+} as Meta<typeof ListItem>
 
 const CustomLink = styled.a`
   color: red;
 `
 
-export const Basic: Story<ListItemProps> = () => {
-  const [checked, setChecked] = useState(false)
-  const handleCheck = () => {
-    setChecked((v) => !v)
-  }
-  return (
-    <>
-      <ListItem>Item</ListItem>
-      <ListItem>
-        <Icon name="16/Add" /> Add
-      </ListItem>
-      <ListItem as="a" href="#">
-        Normal Link
-      </ListItem>
-      <ListItem as={CustomLink} href="#">
-        Custom Link
-      </ListItem>
-      <ListItem onClick={handleCheck}>
-        Switch
-        <div
-          style={{
-            marginLeft: 'auto',
-          }}
-        >
-          <Switch
-            label="hello"
-            name="hello"
-            onChange={handleCheck}
-            checked={checked}
-          />
-        </div>
-      </ListItem>
-    </>
-  )
+export const Basic: StoryObj<typeof ListItem> = {
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [checked, setChecked] = useState(false)
+    const handleCheck = () => {
+      setChecked((v) => !v)
+    }
+    return (
+      <>
+        <ListItem>Item</ListItem>
+        <ListItem>
+          <Icon name="16/Add" /> Add
+        </ListItem>
+        <ListItem as="a" href="#">
+          Normal Link
+        </ListItem>
+        <ListItem as={CustomLink} href="#">
+          Custom Link
+        </ListItem>
+        <ListItem onClick={handleCheck}>
+          Switch
+          <div
+            style={{
+              marginLeft: 'auto',
+            }}
+          >
+            <Switch
+              label="hello"
+              name="hello"
+              onChange={handleCheck}
+              checked={checked}
+            />
+          </div>
+        </ListItem>
+      </>
+    )
+  },
 }
