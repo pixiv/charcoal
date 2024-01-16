b303b12d254ddf2f46eeb53a00ca2a16848edac7
mimo <mimo@pixiv.co.jp>
2024-01-16 18:57:43 +0900

    feat: add tailwind-config spacing docs

---
+++ docs/src/pages/@charcoal-ui/tailwind-config/spacing.page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import { Spacing } from '../../../components/tailwind-config/spacing/Spacing'
+
+const SpacingPage: NextPage = () => (
+  <div className="w-full m-16">
+    <Spacing />
+  </div>
+)
+export default SpacingPage
