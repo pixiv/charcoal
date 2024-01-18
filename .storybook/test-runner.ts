ee167f088c1dc498d433ff26ee52ae1cc4d8a659
naporin0624 <naporin0624@gmail.com>
2024-01-18 19:58:58 +0900

    chore: remove config

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,14 +1,7 @@
 import type { TestRunnerConfig } from '@storybook/test-runner'
 
 const config: TestRunnerConfig = {
-  async postVisit(page) {
-    const elementHandler = await page.$('html')
-    if (elementHandler === null) throw new Error('element not found')
-
-    const innerHTML = await elementHandler.innerHTML()
-
-    expect(innerHTML).toMatchSnapshot()
-  },
+  //
 }
 
 export default config
