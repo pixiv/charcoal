16f70b0d2801ea76e7ae1b0c568261050d61ab7a
mimo <mimo@pixiv.co.jp>
2024-02-08 16:48:22 +0900

    feat: add test-runner image snapshot test

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,7 +1,23 @@
-import type { TestRunnerConfig } from '@storybook/test-runner'
+import { waitForPageReady, type TestRunnerConfig } from '@storybook/test-runner'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
 const config: TestRunnerConfig = {
-  //
+  setup() {
+    expect.extend({ toMatchImageSnapshot })
+  },
+  async postVisit(page, context) {
+    // use the test-runner utility to wait for fonts to load, etc.
+    await waitForPageReady(page)
+
+    // If you want to take screenshot of multiple browsers, use
+    // page.context().browser().browserType().name() to get the browser name to prefix the file name
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot({
+      customSnapshotIdentifier: context.id,
+      failureThreshold: 0.001,
+    })
+  },
 }
 
 export default config
