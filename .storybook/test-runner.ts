2242d3db74bda7ea211d50b4c3e5096ab5d47604
mimo <mimo@pixiv.co.jp>
2024-03-05 17:28:21 +0900

    feat: add test-runner config for image snapshot

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,7 +1,25 @@
-import type { TestRunnerConfig } from '@storybook/test-runner'
+import { waitForPageReady, type TestRunnerConfig } from '@storybook/test-runner'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
 const config: TestRunnerConfig = {
-  //
+  setup() {
+    expect.extend({ toMatchImageSnapshot })
+  },
+  async postVisit(page, context) {
+    await waitForPageReady(page)
+    const image = await page.screenshot()
+    console.log(`${process.cwd()}/__image_snapshots__`)
+    expect(image).toMatchImageSnapshot({
+      customSnapshotIdentifier: context.id,
+      customDiffDir: `${process.cwd()}/__diff_output__`,
+      customSnapshotsDir: `${process.cwd()}/__image_snapshots__`,
+      failureThresholdType: 'percent',
+      failureThreshold: 0.001,
+    })
+  },
+  tags: {
+    skip: ['skip-test'],
+  },
 }
 
 export default config
