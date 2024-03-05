308b2cb283aed1b0949e11aa25080d686c3bbb7b
toshusai <toshusai.net@gmail.com>
2024-03-05 17:54:45 +0900

    test: add a theme utils test

--- packages/styled/src/index.test.tsx
+++ packages/styled/src/index.test.tsx
@@ -2,9 +2,9 @@ import { light } from '@charcoal-ui/theme'
 import 'jest-styled-components'
 
 import renderer from 'react-test-renderer'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { Example } from './index.story'
-import { myTheme } from './storyHelper'
+import { MyTheme, myTheme } from './storyHelper'
 
 function render(children: JSX.Element) {
   return renderer
@@ -18,3 +18,50 @@ describe('Story', () => {
     expect(render(<Example />)).toMatchSnapshot()
   })
 })
+
+describe('ThemeUtilsTest', () => {
+  test('<ThemeUtilsTest />', () => {
+    expect(
+      render(
+        <ThemeUtilsTest>
+          <Typography12 />
+          <Typography14True />
+          <Typography16TrueTrue />
+        </ThemeUtilsTest>
+      )
+    ).toMatchSnapshot()
+  })
+})
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends MyTheme {}
+}
+
+const ThemeUtilsTest = styled.div`
+  ${({ theme }) => theme.utils.margin(0, 4, 8, 'auto')}
+  ${({ theme }) => theme.utils.marginTop(0)}
+  ${({ theme }) => theme.utils.marginRight(4)}
+  ${({ theme }) => theme.utils.marginBottom(8)}
+  ${({ theme }) => theme.utils.marginRight(16)}
+  ${({ theme }) => theme.utils.padding(0, 4, 8, 'auto')}
+  ${({ theme }) => theme.utils.paddingTop(0)}
+  ${({ theme }) => theme.utils.paddingRight(4)}
+  ${({ theme }) => theme.utils.paddingBottom(8)}
+  ${({ theme }) => theme.utils.paddingRight(16)}
+  ${({ theme }) => theme.utils.gap(0, 4)}
+  ${({ theme }) => theme.utils.rowGap(0)}
+  ${({ theme }) => theme.utils.columnGap(4)}
+  ${({ theme }) => theme.utils.focusVisibleFocusRingCss}
+  ${({ theme }) => theme.utils.assertiveRingCss}
+  ${({ theme }) => theme.utils.disabledCss}
+`
+
+const Typography12 = styled.div`
+  ${({ theme }) => theme.utils.typography(12)}
+`
+const Typography14True = styled.div`
+  ${({ theme }) => theme.utils.typography(14, true)}
+`
+const Typography16TrueTrue = styled.div`
+  ${({ theme }) => theme.utils.typography(16, true, true)}
+`
