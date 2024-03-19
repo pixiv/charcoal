132a6de3603e4ad05ac4b4371fa7eafc95e71ed8
toshusai <toshusai.net@gmail.com>
2024-03-19 17:52:08 +0900

    test: add test case

---
+++ packages/react/src/components/Button/index.test.tsx
@@ -0,0 +1,32 @@
+import 'jest-styled-components'
+
+import renderer from 'react-test-renderer'
+import Button from '.'
+
+function Link({
+  as,
+  children,
+  ...props
+}: { as: string; children: React.ReactNode } & React.ComponentProps<'a'>) {
+  return (
+    <a {...props} data-as={as}>
+      {children}
+    </a>
+  )
+}
+
+describe('<Button />', () => {
+  test('componentAs props are passed to the data-as attribute', () => {
+    expect(
+      renderer.create(
+        <Button
+          href="/"
+          target="_blank"
+          fullWidth
+          as={Link}
+          componentAs="componentAs"
+        />
+      )
+    ).toMatchSnapshot()
+  })
+})
