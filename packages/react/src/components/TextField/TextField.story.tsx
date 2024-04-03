39e203c51186786307964a869c84f16acb122649
naporin0624 <naporin0624@gmail.com>
2024-04-03 06:12:49 +0000

    chore(react): TextField Story

--- packages/react/src/components/TextField/TextField.story.tsx
+++ packages/react/src/components/TextField/TextField.story.tsx
@@ -9,24 +9,8 @@ import { Meta, StoryObj } from '@storybook/react'
 export default {
   title: 'TextField',
   component: TextField,
-  argTypes: {},
-  args: {
-    showLabel: false,
-    assistiveText: '',
-    disabled: false,
-    required: false,
-    invalid: false,
-    label: 'Label',
-    requiredText: '*必須',
-    subLabel: <Clickable>Text Link</Clickable>,
-    placeholder: 'TextField',
-  },
-  render: function Render(args) {
-    return (
-      <Container>
-        <TextField {...args} />
-      </Container>
-    )
+  parameters: {
+    layout: 'centered',
   },
 } as Meta<typeof TextField>
 
@@ -35,7 +19,150 @@ const Container = styled.div`
   gap: ${({ theme }) => px(theme.spacing[24])};
 `
 
-export const Default = {}
+export const Default: StoryObj<typeof TextField> = {
+  args: {
+    showLabel: false,
+    assistiveText: '',
+    disabled: false,
+    required: false,
+    invalid: false,
+    readOnly: false,
+    label: 'Label',
+    requiredText: '*必須',
+    subLabel: <Clickable>Text Link</Clickable>,
+    placeholder: 'TextField',
+  },
+  render(args) {
+    return (
+      <Container>
+        <TextField {...args} />
+      </Container>
+    )
+  },
+}
+
+export const Label: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField showLabel label="Label" />
+      </Container>
+    )
+  },
+}
+
+export const Placeholder: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" placeholder="Placeholder" />
+      </Container>
+    )
+  },
+}
+
+export const RequiredText: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" showLabel required requiredText="*必須" />
+      </Container>
+    )
+  },
+}
+
+export const AssistiveText: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" assistiveText="Assistive Text" />
+      </Container>
+    )
+  },
+}
+
+export const SubLabel: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" subLabel={<Clickable>Text Link</Clickable>} />
+      </Container>
+    )
+  },
+}
+
+export const ShowCount = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" showCount maxLength={100} />
+      </Container>
+    )
+  },
+}
+
+export const Disabled: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" disabled />
+      </Container>
+    )
+  },
+}
+
+export const Invalid: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" invalid assistiveText="error message" />
+      </Container>
+    )
+  },
+}
+
+export const ReadOnly: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" readOnly value="読み取り専用" />
+      </Container>
+    )
+  },
+}
+
+export const Affix: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField label="Label" prefix="/home/john/" suffix=".png" />
+      </Container>
+    )
+  },
+}
+
+const PrefixIconWrap = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.color.text3};
+`
+
+export const Prefix: StoryObj<typeof TextField> = {
+  render() {
+    return (
+      <Container>
+        <TextField
+          label="Label"
+          prefix={
+            <PrefixIconWrap>
+              <pixiv-icon name="16/Search" />
+            </PrefixIconWrap>
+          }
+        />
+      </Container>
+    )
+  },
+}
 
 export const Number: StoryObj<typeof TextField> = {
   render: function Render(args) {
@@ -53,70 +180,6 @@ export const Number: StoryObj<typeof TextField> = {
           }}
         />
       </Container>
-    )
-  },
-}
-
-export const HasLabel = {
-  args: {
-    showLabel: true,
-    assistiveText: 'Assistive text',
-    required: true,
-  },
-}
-
-export const HasCount = {
-  args: {
-    showCount: true,
-    maxLength: 100,
-  },
-}
-
-export const HasAffix: StoryObj<typeof TextField> = {
-  args: {
-    showCount: true,
-    maxLength: 200,
-    prefix: '/home/john/',
-    suffix: '.png',
-  },
-}
-
-const PrefixIconWrap = styled.div`
-  display: flex;
-  align-items: center;
-  color: ${({ theme }) => theme.color.text3};
-`
-
-export const PrefixIcon: StoryObj<typeof TextField> = {
-  render: function Render(args) {
-    const [value, setValue] = useState('')
-    const handleChange = useCallback((value: string) => {
-      setValue(value)
-    }, [])
-    const handleClear = useCallback(() => {
-      setValue('')
-    }, [])
-    return (
-      <TextField
-        {...args}
-        label="Label"
-        placeholder="作品を検索"
-        value={value}
-        onChange={handleChange}
-        prefix={
-          <PrefixIconWrap>
-            <pixiv-icon name="16/Search" />
-          </PrefixIconWrap>
-        }
-        suffix={
-          <IconButton
-            variant="Overlay"
-            icon={'16/Remove'}
-            size="XS"
-            onClick={handleClear}
-          />
-        }
-      />
     )
   },
 }
