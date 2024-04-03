db1b11e0223abaeb17a9b8e16979b96f58362429
naporin0624 <naporin0624@gmail.com>
2024-04-03 05:39:39 +0000

    chore(react): add TextArea story

--- packages/react/src/components/TextArea/TextArea.story.tsx
+++ packages/react/src/components/TextArea/TextArea.story.tsx
@@ -8,7 +8,17 @@ import { Meta, StoryObj } from '@storybook/react'
 export default {
   title: 'TextArea',
   component: TextArea,
-  argTypes: {},
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof TextArea>
+
+const Container = styled.div`
+  display: grid;
+  gap: ${({ theme }) => px(theme.spacing[24])};
+`
+
+export const Default: StoryObj<typeof TextArea> = {
   args: {
     showLabel: false,
     label: 'Label',
@@ -27,32 +37,80 @@ export default {
       </Container>
     )
   },
-} as Meta<typeof TextArea>
+}
 
-const Container = styled.div`
-  display: grid;
-  gap: ${({ theme }) => px(theme.spacing[24])};
-`
-
-export const Default: StoryObj<typeof TextArea> = {}
-
-export const HasLabel: StoryObj<typeof TextArea> = {
-  args: {
-    showLabel: true,
-    assistiveText: 'Assistive text',
-    required: true,
+export const ShowLabel: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea showLabel required label="Label" />
+      </Container>
+    )
   },
 }
 
-export const HasCount: StoryObj<typeof TextArea> = {
-  args: {
-    showCount: true,
-    maxLength: 100,
+export const AssistiveText: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea assistiveText="Assistive text" label="Label" />
+      </Container>
+    )
+  },
+}
+
+export const ShowCount: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea showCount maxLength={100} label="Label" />
+      </Container>
+    )
+  },
+}
+
+export const Invalid: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea invalid label="Label" />
+      </Container>
+    )
+  },
+}
+
+export const SubLabel: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea
+          showLabel
+          label="Label"
+          subLabel={
+            <Clickable onClick={action('label-click')}>Text Link</Clickable>
+          }
+        />
+      </Container>
+    )
+  },
+}
+
+export const Disabled: StoryObj<typeof TextArea> = {
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea disabled label="Label" />
+      </Container>
+    )
   },
 }
 
 export const AutoHeight: StoryObj<typeof TextArea> = {
-  args: {
-    autoHeight: true,
+  render: function Render() {
+    return (
+      <Container>
+        <TextArea autoHeight label="Label" />
+      </Container>
+    )
   },
 }
