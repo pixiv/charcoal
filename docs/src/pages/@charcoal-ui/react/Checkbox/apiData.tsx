c81796956809a7b8fcdb22b770856d1454eaa2c2
toshusai <toshusai.net@gmail.com>
2024-04-11 16:22:42 +0900

    docs: fix type error

--- docs/src/pages/@charcoal-ui/react/Checkbox/apiData.tsx
+++ docs/src/pages/@charcoal-ui/react/Checkbox/apiData.tsx
@@ -1,30 +1,11 @@
 import { CheckboxProps } from '@charcoal-ui/react'
-import { ApiTableData, TableItem } from '../_components/ApiTable'
+import { ComponentPropsWithRef } from 'react'
+import { ApiTableData } from '../_components/ApiTable'
 
 export const apiData: Omit<
   ApiTableData<CheckboxProps, HTMLInputElement>,
-  'type' | 'name'
-> & { label: TableItem } = {
-  disabled: {
-    description: '入力の無効化',
-    type: 'boolean',
-    default: 'false',
-  },
-  label: {
-    description: 'childrenがない場合のaria-label',
-    type: 'string',
-    required: true,
-  },
-  readonly: {
-    description: '読み取り専用化',
-    default: 'false',
-    type: 'boolean',
-  },
-  checked: {
-    description: 'チェック状態',
-    default: 'false',
-    type: 'boolean',
-  },
+  keyof ComponentPropsWithRef<'input'>
+> = {
   invalid: {
     description: '入力の不正化',
     default: 'false',
