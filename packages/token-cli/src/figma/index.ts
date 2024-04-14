e4d02104826c9ee155c9f694dea53d92cf11115b
mimo <mimo@pixiv.co.jp>
2024-04-15 07:41:43 +0900

    chore: chore

--- packages/token-cli/src/figma/index.ts
+++ packages/token-cli/src/figma/index.ts
@@ -9,5 +9,132 @@ const api = (pat: string) =>
     baseURL: 'https://api.figma.com',
   })
 
+// https://www.figma.com/developers/api#variables-types
+type VariableAlias = {
+  type: 'VARIABLE_ALIAS'
+  id: string
+}
+
+type VariableCodeSyntax = {
+  WEB: string
+  ANDROID: string
+  iOS: string
+}
+
+type Color = {
+  r: number
+  g: number
+  b: number
+  a: number
+}
+
+type Scope =
+  | 'ALL_SCOPES'
+  | 'CORNER_RADIUS'
+  | 'TEXT_CONTENT'
+  | 'WIDTH_HEIGHT'
+  | 'GAP'
+  | 'STROKE_FLOAT'
+  | 'OPACITY'
+  | 'EFFECT_FLOAT'
+  | 'ALL_FILLS'
+  | 'FRAME_FILL'
+  | 'SHAPE_FILL'
+  | 'TEXT_FILL'
+  | 'STROKE_COLOR'
+  | 'EFFECT_COLOR'
+
+type ResolveType = 'BOOLEAN' | 'FLOAT' | 'STRING' | 'COLOR'
+
+export type VariableCollection = {
+  defaultModeId: string
+  id: string
+  name: string
+  remote: boolean
+  modes: { modeId: string; name: string }[]
+  key: string
+  hiddenFromPublishing: boolean
+  variableIds: string[]
+}
+
+export type Variable = {
+  id: string
+  name: string
+  remote: boolean
+  key: string
+  variableCollectionId: string
+  resolvedType: ResolveType
+  description: string
+  hiddenFromPublishing: false
+  valuesByMode: {
+    [key: string]: VariableAlias | Color | boolean | number | string
+  }
+  scopes: Scope[]
+  codeSyntax: VariableCodeSyntax
+}
+
+export type FigmaResponse = {
+  status: number
+  error: boolean
+  meta: {
+    variableCollections: {
+      [key: string]: VariableCollection
+    }
+    variables: {
+      [key: string]: Variable
+    }
+  }
+}
+
+const colorToRgba = ({ r, g, b, a }: Color) =>
+  `rgba(${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(
+    b * 255
+  )}, ${Math.round(a * 1000) / 1000})`
+
 export const getDesignToken = (pat: string, nodeId: string) =>
   api(pat).get(`/v1/files/${nodeId}/variables/local`)
+
+export const resolveValue = (
+  variableCollectionMap: Map<string, VariableCollection>,
+  variableMap: Map<string, Variable>,
+  resolvedType: ResolveType,
+  variable: Variable,
+  modeId: string
+): string => {
+  const value = variable.valuesByMode[modeId]
+
+  // aliasだった場合は再起的にみる
+  if (typeof value === 'object' && 'type' in value && 'id' in value) {
+    const v = variableMap.get(value.id)
+    if (!v) throw new Error(`can't find variable ${value.id}`)
+
+    const variableCollection = variableCollectionMap.get(v.variableCollectionId)
+
+    if (!variableCollection)
+      throw new Error(
+        `can't find variable collection ${v.variableCollectionId}`
+      )
+
+    return resolveValue(
+      variableCollectionMap,
+      variableMap,
+      resolvedType,
+      v,
+      variableCollection.defaultModeId
+    )
+  }
+
+  switch (resolvedType) {
+    case 'COLOR':
+      return resolveColor(value as Color)
+    case 'FLOAT':
+      return resolveFloat(value as number)
+
+    default:
+      throw new Error('unsupport resolvedType')
+  }
+}
+
+export const resolveColor = (value: Color) => colorToRgba(value)
+
+export const resolveFloat = (value: number) => `${value}rem`
--- packages/token-cli/src/index.ts
+++ packages/token-cli/src/index.ts
@@ -1,7 +1,13 @@
-import { ensureDir, writeFile } from 'fs-extra'
+import { ensureDir, readFileSync, writeFile } from 'fs-extra'
 import path from 'path'
 import yargs from 'yargs'
-import { getDesignToken } from './figma'
+import {
+  FigmaResponse,
+  getDesignToken,
+  resolveValue,
+  Variable,
+  VariableCollection,
+} from './figma'
 import { mustBeDefined } from './utils'
 
 /**
@@ -12,18 +18,129 @@ const FIGMA_NODE_ID = process.env.FIGMA_NODE_ID
 
 void yargs
   .scriptName('token-cli')
-  .command('test', 'test figma variables api', {}, async () => {
+  .command('fetch:primitive', 'Fetch Figma variables', {}, async () => {
     mustBeDefined(FIGMA_TOKEN, 'FIGMA_TOKEN')
     mustBeDefined(FIGMA_NODE_ID, 'FIGMA_NODE_ID')
 
     const res = await getDesignToken(FIGMA_TOKEN, FIGMA_NODE_ID)
 
-    console.dir(res)
+    await ensureDir(path.join(__dirname, '..', 'out'))
+    await writeFile(
+      path.join(__dirname, '..', 'out', 'raw_primitives.json'),
+      JSON.stringify(res.data),
+      'utf8'
+    )
+  })
+  .command('fetch:applied', 'Fetch Figma variables', {}, async () => {
+    mustBeDefined(FIGMA_TOKEN, 'FIGMA_TOKEN')
+    mustBeDefined(FIGMA_NODE_ID, 'FIGMA_NODE_ID')
+
+    const res = await getDesignToken(FIGMA_TOKEN, FIGMA_NODE_ID)
 
     await ensureDir(path.join(__dirname, '..', 'out'))
     await writeFile(
-      path.join(__dirname, '..', 'out', 'tokens.json'),
+      path.join(__dirname, '..', 'out', 'raw_applieds.json'),
       JSON.stringify(res.data),
+      'utf8'
+    )
+  })
+  .command('transform', 'Transform Fetched variables', {}, async () => {
+    const buffer = readFileSync(
+      path.join(__dirname, '..', 'out', 'raw_primitives.json')
+    )
+    const tokens = JSON.parse(buffer.toString()) as FigmaResponse
+
+    const variableMap = new Map<string, Variable>()
+    Object.entries(tokens.meta.variables).forEach(([key, it]) => {
+      variableMap.set(key, it)
+    })
+
+    const variableCollectionMap = new Map<string, VariableCollection>()
+    Object.entries(tokens.meta.variableCollections).forEach(([key, it]) =>
+      variableCollectionMap.set(key, it)
+    )
+
+    const primitives = Object.entries(tokens.meta.variableCollections)
+      .filter(([_, it]) => !it.remote)
+      .map(([_, collection]) => {
+        const variables = collection.variableIds
+          .filter((it) => variableMap.get(it))
+          .map((it) => {
+            const v = variableMap.get(it)
+
+            if (!v) throw new Error(`can't find variable: ${it}`)
+
+            return {
+              [v.name]: {
+                value: resolveValue(
+                  variableCollectionMap,
+                  variableMap,
+                  v.resolvedType,
+                  v,
+                  collection.defaultModeId
+                ),
+              },
+            }
+          })
+          .reduce((prev, current) => ({ ...prev, ...current }))
+        return { [collection.name]: variables }
+      })
+      .reduce((prev, current) => ({ ...prev, ...current }))
+
+    await ensureDir(path.join(__dirname, '..', 'out'))
+    await writeFile(
+      path.join(__dirname, '..', 'out', 'transformed.json'),
+      JSON.stringify(primitives),
+      'utf8'
+    )
+  })
+  .command('transform:applied', 'Transform Fetched variables', {}, async () => {
+    const buffer = readFileSync(
+      path.join(__dirname, '..', 'out', 'raw_applieds.json')
+    )
+    const tokens = JSON.parse(buffer.toString()) as FigmaResponse
+
+    const variableMap = new Map<string, Variable>()
+    Object.entries(tokens.meta.variables).forEach(([key, it]) => {
+      variableMap.set(key, it)
+    })
+
+    const variableCollectionMap = new Map<string, VariableCollection>()
+    Object.entries(tokens.meta.variableCollections).forEach(([key, it]) =>
+      variableCollectionMap.set(key, it)
+    )
+
+    const primitives = Object.entries(tokens.meta.variableCollections)
+      .filter(([_, it]) => !it.remote)
+      .map(([_, collection]) => {
+        const variables = collection.variableIds
+          .filter((it) => variableMap.get(it))
+          .map((it) => {
+            const v = variableMap.get(it)
+
+            if (!v) throw new Error(`can't find variable: ${it}`)
+
+            return {
+              [v.name]: {
+                value: resolveValue(
+                  variableCollectionMap,
+                  variableMap,
+                  v.resolvedType,
+                  v,
+                  collection.defaultModeId
+                ),
+              },
+            }
+          })
+          .reduce((prev, current) => ({ ...prev, ...current }))
+        return { [collection.name]: variables }
+      })
+      .reduce((prev, current) => ({ ...prev, ...current }))
+
+    await ensureDir(path.join(__dirname, '..', 'out'))
+    await writeFile(
+      path.join(__dirname, '..', 'out', 'transformed.json'),
+      JSON.stringify(primitives),
       'utf8'
     )
   })
