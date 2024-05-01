68f5ddfe37c3e59d586d11af63d23021e43ff225
naporin0624 <naporin0624@gmail.com>
2024-05-01 07:01:33 +0000

    feat(react): update clickable v4

--- packages/react/src/components/Clickable/index.tsx
+++ packages/react/src/components/Clickable/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import {
   LinkProps,
   useComponentAbstraction,
 } from '../../core/ComponentAbstraction'
-import { disabledSelector } from '@charcoal-ui/utils'
+import { useClassNames } from '../../_lib/useClassNames'
 
 interface BaseProps {
   /**
@@ -27,65 +26,25 @@ export type ClickableElement = HTMLButtonElement & HTMLAnchorElement
 
 const Clickable = React.forwardRef<ClickableElement, ClickableProps>(
   function Clickable(props, ref) {
+    const className = useClassNames('charcoal-clickable', props.className)
+
     const { Link } = useComponentAbstraction()
     const isLink = 'to' in props
-    const as = isLink ? Link : 'button'
-    const ariaDisabled = isLink && props.disabled === true ? true : undefined
-    let rest = props
+
     if (isLink) {
       const { disabled, ..._rest } = props
-      rest = _rest
+
+      return (
+        <Link
+          {..._rest}
+          ref={ref}
+          aria-disabled={disabled === true ? true : undefined}
+          className={className}
+        />
+      )
     }
-    return (
-      <StyledClickableDiv
-        {...rest}
-        ref={ref}
-        as={as}
-        aria-disabled={ariaDisabled}
-      />
-    )
+
+    return <button {...props} ref={ref} className={className} />
   }
 )
 export default Clickable
-
-const StyledClickableDiv = styled.div`
-  cursor: pointer;
-
-  ${disabledSelector} {
-    cursor: default;
-  }
-
-  /* Reset button appearance */
-  appearance: none;
-  background: transparent;
-  padding: 0;
-  border-style: none;
-  outline: none;
-  color: inherit;
-  text-rendering: inherit;
-  letter-spacing: inherit;
-  word-spacing: inherit;
-  text-decoration: none;
-
-  &:focus {
-    outline: none;
-  }
-
-  /* Change the font styles in all browsers. */
-  font: inherit;
-
-  /* Remove the margin in Firefox and Safari. */
-  margin: 0;
-
-  /* Show the overflow in Edge. */
-  overflow: visible;
-
-  /* Remove the inheritance of text transform in Firefox. */
-  text-transform: none;
-
-  /* Remove the inner border and padding in Firefox. */
-  &::-moz-focus-inner {
-    border-style: none;
-    padding: 0;
-  }
-`
