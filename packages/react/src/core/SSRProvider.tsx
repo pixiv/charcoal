7cd44572948b74283d3384046fc28bb865c94065
yue <yue@pixiv.co.jp>
2024-06-04 22:14:51 +0900

    docs: add comment for react version history

--- packages/react/src/core/SSRProvider.tsx
+++ packages/react/src/core/SSRProvider.tsx
@@ -2,6 +2,7 @@ import { SSRProvider as OriginSSRProvider } from '@react-aria/ssr'
 import { version, Fragment } from 'react'
 
 export function isReactVersionOver(minVersion: number): boolean {
+  // version history on the react side: https://github.com/facebook/react/commits/main/packages/shared/ReactVersion.js
   const reactMajorVersion = parseInt(version.split('.')[0], 10)
   return Number.isFinite(reactMajorVersion)
     ? reactMajorVersion >= minVersion
