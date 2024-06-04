e2840816ef8ed8c8e9fa8ccf52b63f350c89f5ac
mimo <mimo@pixiv.co.jp>
2024-06-04 15:48:40 +0900

    feat(token-cli): custom formatter

---
+++ packages/token-cli/pixiv-light.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+module.exports = {
+  source: ['tokens/base.json', 'tokens/pixiv-light.json'],
+  transform: {
+    'name/cti/kebab': {
+      type: 'name',
+      transformer: (token) => {
+        return token.path
+          .join('-')
+          .toLowerCase()
+          .replaceAll('/', '-')
+          .replaceAll(' ', '-')
+      },
+    },
+  },
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'build/css/',
+      prefix: 'charcoal',
+      files: [
+        {
+          destination: '_variables_light.css',
+          format: 'css/variables',
+        },
+      ],
+      options: {
+        outputReferences: true,
+      },
+    },
+  },
+}
