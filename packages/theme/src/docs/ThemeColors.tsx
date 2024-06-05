2a4978ca2a1dd1f71eafe15a823e03c21f92135b
naporin0624 <naporin0624@gmail.com>
2024-06-05 10:41:32 +0000

    chore(theme): migrate docs

---
+++ packages/theme/src/docs/ThemeColors.tsx
@@ -0,0 +1,65 @@
+import { light, dark, ThemeColor } from '..'
+import { useDarkMode } from 'storybook-dark-mode'
+
+type ColorProps = keyof ThemeColor
+
+const bgList: ColorProps[] = [
+  'background1',
+  'background2',
+  'surface1',
+  'surface2',
+  'surface3',
+  'surface4',
+  'surface6',
+  'surface7',
+  'surface8',
+  'surface9',
+  'surface10',
+]
+
+const textList: ColorProps[] = [
+  'text1',
+  'text2',
+  'text3',
+  'text4',
+  'text5',
+  'icon6',
+  'link1',
+  'link2',
+]
+
+export const ThemeColors = () => {
+  const isDark = useDarkMode()
+  const theme = isDark ? dark : light
+  theme.color.background1
+  return (
+    <div
+      style={{
+        fontFamily:
+          "'SF Mono', SFMono-Regular, ui-monospace, 'DejaVu Sans Mono', Menlo, Consolas, monospace",
+      }}
+    >
+      {bgList.map((bg) => (
+        <div
+          key={bg}
+          style={{
+            color: theme.color.text1,
+            backgroundColor: theme.color[bg],
+          }}
+        >
+          {bg} {theme.color[bg]}
+          {textList.map((text) => (
+            <div
+              key={text}
+              style={{
+                color: theme.color[text],
+              }}
+            >
+              {text} {theme.color[text]}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
