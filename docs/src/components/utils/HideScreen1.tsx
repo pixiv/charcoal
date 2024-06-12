34aea10e280b784c080305e0263026a774c52cfe
naporin0624 <naporin0624@gmail.com>
2024-06-12 07:59:35 +0000

    chore: remove next.js docs

--- docs/src/components/utils/HideScreen1.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { maxWidth } from '@charcoal-ui/utils'
-
-export const HideScreen1 = styled.div`
-  display: none;
-  @media ${({ theme }) => maxWidth(theme.breakpoint.screen1)} {
-    display: inherit;
-  }
-`
