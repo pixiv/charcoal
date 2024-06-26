98d116cdeb1b7dae4a70f8776c670eb168e05e69
naporin0624 <naporin0624@gmail.com>
2024-06-26 09:28:21 +0000

    chore: a11y testing test-runner

--- packages/react/jest.config.mjs
+++ packages/react/jest.config.mjs
@@ -2,7 +2,12 @@ import { config } from '../../jest.config.mjs'
 
 const isCI = process.env.CI === 'true'
 
+const c = config()
 export default {
-  ...config(),
+  ...c,
+  moduleNameMapper: {
+    ...(c.moduleNameMapper !== undefined ? { ...c.moduleNameMapper } : {}),
+    '^@storybook/react-dom-shim$': '@storybook/react-dom-shim/dist/react-16',
+  },
   testPathIgnorePatterns: isCI ? ['/src/.*performance\\.test\\.tsx$'] : [],
 }
