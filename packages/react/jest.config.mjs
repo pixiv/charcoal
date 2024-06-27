7750fe91261d5b01776c69ece387f83f32e096b6
naporin0624 <naporin0624@gmail.com>
2024-06-27 11:02:43 +0000

    chore(react): also revert the jest config settings

--- packages/react/jest.config.mjs
+++ packages/react/jest.config.mjs
@@ -2,12 +2,7 @@ import { config } from '../../jest.config.mjs'
 
 const isCI = process.env.CI === 'true'
 
-const c = config()
 export default {
-  ...c,
-  moduleNameMapper: {
-    ...(c.moduleNameMapper !== undefined ? { ...c.moduleNameMapper } : {}),
-    '^@storybook/react-dom-shim$': '@storybook/react-dom-shim/dist/react-16',
-  },
+  ...config(),
   testPathIgnorePatterns: isCI ? ['/src/.*performance\\.test\\.tsx$'] : [],
 }
