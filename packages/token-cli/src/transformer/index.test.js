3b2c60e5f8ca564158f2954b621a4a6bb2e6b471
mimo <mimo@pixiv.co.jp>
2024-07-08 13:27:10 +0900

    chore(token-cli): add test

---
+++ packages/token-cli/src/transformer/index.test.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { transformer } = require('.')
+test('tests transformer real case', () => {
+  expect(transformer({ path: ['Color', 'container/secondary/defaultA'] })).toBe(
+    'color-container-secondary-default-a'
+  )
+})
+test('tests transformer unreal case', () => {
+  expect(transformer({ path: ['Color', 'Container/Secondary/DefaultABCDEF'] })).toBe(
+    'color-container-secondary-default-a-b-c-d-e-f'
+  )
+})
