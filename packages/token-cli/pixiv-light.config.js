8051f38a785d9252c0c2d0cb29f6a11c3f1c601a
mimo <mimo@pixiv.co.jp>
2024-07-08 13:25:53 +0900

    fix(token-cli): camel case to cebab case

--- packages/token-cli/pixiv-light.config.js
+++ packages/token-cli/pixiv-light.config.js
@@ -7,6 +7,7 @@ module.exports = {
       transformer: (token) => {
         return token.path
           .join('-')
+          .replace(/(.)([A-Z])/g, '$1-$2')
           .toLowerCase()
           .replaceAll('/', '-')
           .replaceAll(' ', '-')
