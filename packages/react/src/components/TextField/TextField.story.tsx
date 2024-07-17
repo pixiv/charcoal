3a1cc80d3ae500adc27dfc8b39986ac5b575d6e0
naporitan <naporin0624@gmail.com>
2024-07-17 17:56:47 +0900

    feat(react): replace new tokens

--- packages/react/src/components/TextField/TextField.story.tsx
+++ packages/react/src/components/TextField/TextField.story.tsx
@@ -101,7 +101,7 @@ export const Prefix: StoryObj<typeof TextField> = {
             style={{
               display: 'flex',
               alignItems: 'center',
-              color: 'var(--charcoal-color-text3)',
+              color: 'var(--charcoal-color-text-tertiary-default)',
             }}
           >
             <pixiv-icon name="16/Search" />
