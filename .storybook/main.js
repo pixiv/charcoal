c2497dbd827872a5b60f26e4b65611761bce7cfd
naporin0624 <naporin0624@gmail.com>
2024-08-07 08:58:38 +0000

    chore: remove react shims settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -52,12 +52,6 @@ module.exports = {
   ],
 
   async webpackFinal(config, { configType }) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: getAbsolutePath('react'),
-      'react-dom': getAbsolutePath('react-dom'),
-    }
-
     if (configType === 'PRODUCTION') {
       return config
     }
