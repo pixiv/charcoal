e589df7260ff795b57d9b1a08fca6a1f04db466a
naporin0624 <naporin0624@gmail.com>
2024-09-11 11:01:31 +0000

    chore(icons): 試しに dynamic import してみる

--- packages/icons/src/charcoalIconFiles.ts
+++ packages/icons/src/charcoalIconFiles.ts
@@ -1,11 +1,9 @@
-import charcoalIconFiles from '@charcoal-ui/icon-files'
+import type charcoalIconFiles from '@charcoal-ui/icon-files'
 
 export default charcoalIconFiles
 export type KnownIconFile = keyof typeof charcoalIconFiles
-export const KNOWN_ICON_FILES = Object.keys(
-  charcoalIconFiles
-) as KnownIconFile[]
+export const KNOWN_ICON_FILES: KnownIconFile[] = []
 
 export function isKnownIconFile(name: string): name is KnownIconFile {
-  return name in charcoalIconFiles
+  return true
 }
