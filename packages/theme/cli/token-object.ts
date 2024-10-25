66e33c63916a1254f808ad6f532975c77d43ba57
naporin0624 <naporin0624@gmail.com>
2024-10-25 14:06:57 +0000

    chore(theme): 一括で生成する

--- packages/theme/cli/token-object.ts
+++ packages/theme/cli/token-object.ts
@@ -1,64 +1,128 @@
 /* eslint-disable no-console */
-import { mkdirSync, readFileSync, writeFileSync } from 'fs'
-import path from 'path'
+import { readFileSync, mkdirSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
 import { createCSSTokenObject, createTokenObject } from '../src/token-object'
 import type { TokenDictionary } from '../src/token-object/types'
-import { parseArgs } from 'node:util'
 import { camelCaseKeys } from '../src/token-object'
 
-// コマンドライン引数の解析
-const args = parseArgs({
-  options: {
-    token: { type: 'string' },
-    base: { type: 'string' },
-    output: { type: 'string' },
-    format: { type: 'string', short: 'f' },
+type ValueStyle = 'cssVariable'
+type KeyStyle = 'camelCase'
+
+type Config = {
+  tokenFile: string
+  baseFile: string
+  outputFile: string
+  keyStyle?: KeyStyle
+  valueStyle?: ValueStyle
+}
+
+const configurations: Config[] = [
+  {
+    tokenFile: './src/json/pixiv-light.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/pixiv-light.json',
+    keyStyle: undefined,
+    valueStyle: undefined,
   },
-})
+  {
+    tokenFile: './src/json/pixiv-dark.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/pixiv-dark.json',
+    keyStyle: undefined,
+    valueStyle: undefined,
+  },
 
-const tokenFilePath = args.values.token
-const baseFilePath = args.values.base
-const outputFilePath = args.values.output
-const format = args.values.format
+  {
+    tokenFile: './src/json/pixiv-light.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/camel/pixiv-light.json',
+    keyStyle: 'camelCase',
+    valueStyle: undefined,
+  },
+  {
+    tokenFile: './src/json/pixiv-dark.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/camel/pixiv-dark.json',
+    keyStyle: 'camelCase',
+    valueStyle: undefined,
+  },
 
-if (
-  tokenFilePath === undefined ||
-  baseFilePath === undefined ||
-  outputFilePath === undefined
-) {
-  console.error('Error: --token, --base, and --output options are required.')
-  process.exit(1)
+  {
+    tokenFile: './src/json/pixiv-light.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/css-variables/pixiv-light.json',
+    keyStyle: undefined,
+    valueStyle: 'cssVariable',
+  },
+  {
+    tokenFile: './src/json/pixiv-dark.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/css-variables/pixiv-dark.json',
+    keyStyle: undefined,
+    valueStyle: 'cssVariable',
+  },
+
+  {
+    tokenFile: './src/json/pixiv-light.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/css-variables/camel/pixiv-light.json',
+    keyStyle: 'camelCase',
+    valueStyle: 'cssVariable',
+  },
+  {
+    tokenFile: './src/json/pixiv-dark.json',
+    baseFile: './src/json/base.json',
+    outputFile: './dist/tokens/css-variables/camel/pixiv-dark.json',
+    keyStyle: 'camelCase',
+    valueStyle: 'cssVariable',
+  },
+]
+
+for (const {
+  tokenFile,
+  baseFile,
+  outputFile,
+  keyStyle,
+  valueStyle,
+} of configurations) {
+  const baseJson = JSON.parse(
+    readFileSync(path.resolve(baseFile), 'utf8')
+  ) as TokenDictionary
+
+  const createToken = <T extends TokenDictionary>(
+    value: T
+  ): Record<string, unknown> => {
+    switch (valueStyle) {
+      case 'cssVariable': {
+        return createCSSTokenObject(value, (x) => `charcoal-${x}`)
+      }
+      default: {
+        return createTokenObject(value, baseJson)
+      }
+    }
+  }
+  const transformKey = <T extends Record<string, unknown>>(
+    value: T
+  ): Record<string, unknown> => {
+    switch (keyStyle) {
+      case 'camelCase': {
+        return camelCaseKeys(value)
+      }
+      default: {
+        return value
+      }
+    }
+  }
+
+  const tokenJson = JSON.parse(
+    readFileSync(path.resolve(tokenFile), 'utf8')
+  ) as TokenDictionary
+  const tokenObject = transformKey(createToken(tokenJson))
+
+  mkdirSync(path.dirname(outputFile), { recursive: true })
+  writeFileSync(path.resolve(outputFile), JSON.stringify(tokenObject, null, 2))
+
+  console.log(
+    `Generated ${outputFile} with keyStyle ${keyStyle} and valueStyle ${valueStyle}.`
+  )
 }
-
-// ファイルの読み込み
-const tokenJson = JSON.parse(
-  readFileSync(path.resolve(tokenFilePath), 'utf8')
-) as TokenDictionary
-const baseJson = JSON.parse(
-  readFileSync(path.resolve(baseFilePath), 'utf8')
-) as TokenDictionary
-
-// トークンオブジェクトの生成
-let tokenObject = {}
-switch (format) {
-  case 'camel': {
-    tokenObject = camelCaseKeys(createTokenObject(tokenJson, baseJson))
-    break
-  }
-  case 'css': {
-    tokenObject = createCSSTokenObject(tokenJson, (x) => `charcoal-${x}`)
-    break
-  }
-  default: {
-    tokenObject = createTokenObject(tokenJson, baseJson)
-    break
-  }
-}
-
-mkdirSync(path.dirname(outputFilePath), { recursive: true })
-writeFileSync(
-  path.resolve(outputFilePath),
-  JSON.stringify(tokenObject, null, 2)
-)
-
-console.log(`Token object generated and saved to ${outputFilePath}`)
