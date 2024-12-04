4a62b4b7575d0b95259cb891e33f10ce5016dfe8
mimo <mimo@pixiv.co.jp>
2024-12-04 14:53:46 +0900

    feat: add style-dictionary-plugins

--- packages/style-dictionary-plugins/tsup.config.ts
+++ packages/style-dictionary-plugins/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: 'node16',
+  target: 'esnext',
   sourcemap: true,
   format: ['cjs', 'esm'],
 })
