d04b70d48564af41c1f1e87308f1c5dcb911a9ec
yue <yue@pixiv.co.jp>
2024-12-13 19:48:30 +0900

    fix: remove extra css import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,6 @@ import themeDecorator from './theme-decorator'
 import { DocsContainer } from './docs-container'
 
 import './global.css'
-import '../packages/react/dist/index.css'
 
 /** @type  */
 export const parameters = {
