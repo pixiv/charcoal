879a132e6f9f3501123becb8645da406cbe0a5a6
naporin0624 <naporin0624@gmail.com>
2024-12-25 09:55:50 +0000

    chore(react-sandbox): simplify type inference with any

--- packages/react-sandbox/src/styled.ts
+++ packages/react-sandbox/src/styled.ts
@@ -1,3 +1,14 @@
 import { createTheme } from '@charcoal-ui/styled'
+import type { ThemeProp } from '@charcoal-ui/styled'
+import type createO from '@charcoal-ui/styled/src/builders/o'
+import type { ArrayOrSingle } from '@charcoal-ui/styled/src/util'
 import type { CharcoalTheme } from '@charcoal-ui/theme'
-export const theme = createTheme<CharcoalTheme>()
+
+type Builder = ReturnType<typeof createO<CharcoalTheme>>
+// 内部的に使っているだけで外部には公開しないので dts コンパイルのために any にして型計算を省略
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type SpecFn = (o: Builder) => ArrayOrSingle<any>
+
+type Theme = (specFn: SpecFn) => ThemeProp<CharcoalTheme>
+
+export const theme: Theme = createTheme<CharcoalTheme>()
--- packages/react-sandbox/tsup.config.ts
+++ packages/react-sandbox/tsup.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   sourcemap: true,
   esbuildPlugins: [styledComponentsPlugin],
   clean: true,
-  dts: false,
+  dts: true,
   tsconfig: './tsconfig.build.json',
 })
