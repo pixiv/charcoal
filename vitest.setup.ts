58d02e53bbc13709d34db0cf2a00c49eb2a771da
naporin0624 <naporin0624@gmail.com>
2025-02-20 07:02:36 +0000

    chore: github actions fix

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -7,31 +7,38 @@ import type { ReactPortal } from 'react'
 setProjectAnnotations(projectAnnotations)
 
 beforeAll(() => {
-  globalThis.ResizeObserver = vi.fn().mockImplementation(() => ({
-    observe: vi.fn(),
-    unobserve: vi.fn(),
-    disconnect: vi.fn(),
-  }))
-
-  global.IntersectionObserver = vi.fn().mockImplementation(() => ({
-    observe() {
-      return null
-    },
-    disconnect() {
-      return null
-    },
-  }))
-
-  window.matchMedia = vi.fn().mockImplementation((query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(),
-    removeListener: vi.fn(),
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  }))
+  vi.stubGlobal(
+    'ResizeObserver',
+    vi.fn().mockImplementation(() => ({
+      observe: vi.fn(),
+      unobserve: vi.fn(),
+      disconnect: vi.fn(),
+    }))
+  )
+  vi.stubGlobal(
+    'IntersectionObserver',
+    vi.fn().mockImplementation(() => ({
+      observe() {
+        return null
+      },
+      disconnect() {
+        return null
+      },
+    }))
+  )
+  vi.stubGlobal(
+    'matchMedia',
+    vi.fn().mockImplementation((query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    }))
+  )
 
   vi.mock('@react-aria/utils', async () => {
     const utils = await vi.importActual('@react-aria/utils')
