8f0a65b0317bcec7983f2b07f5279871b1da25ad
yue4u <yue4u@users.noreply.github.com>
2025-03-05 07:23:21 +0000

    deploy: c9f6946eda1db84cd17613f39d15cf750e6cddcb

---
+++ react-sandbox-src-components-LeftMenu-index-story.b51e7cc4.iframe.bundle.js
@@ -0,0 +1,98 @@
+"use strict";(self.webpackChunkcharcoal_ui=self.webpackChunkcharcoal_ui||[]).push([[8016],{"./packages/react-sandbox/src/components/LeftMenu/index.story.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,default:()=>index_story});var jsx_runtime=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),styled_components_browser_esm=__webpack_require__("./node_modules/.pnpm/styled-components@5.3.11_@babel+core@7.26.8_react-dom@18.3.1_react@18.3.1__react-is@18.3.1_react@18.3.1/node_modules/styled-components/dist/styled-components.browser.esm.js"),react=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),styled=__webpack_require__("./packages/react-sandbox/src/styled.ts");const hasChildren=element=>(0,react.isValidElement)(element)&&Boolean(element.props.children),childToString=child=>null==child||"boolean"==typeof child||"{}"===JSON.stringify(child)?"":child.toString(),onlyText=children=>Array.isArray(children)||(0,react.isValidElement)(children)?react.Children.toArray(children).reduce(((text,child)=>{let newText="";return newText=(0,react.isValidElement)(child)&&hasChildren(child)?onlyText(child.props.children):(0,react.isValidElement)(child)&&!hasChildren(child)?"":childToString(child),text.concat(newText)}),""):childToString(children),TextEllipsis=styled_components_browser_esm.Ay.div.attrs((({children,title=onlyText(children)})=>({title:""!==title?title:void 0})))`
+  overflow: hidden;
+  line-height: ${props=>props.lineHeight}px;
+  /* For english */
+  overflow-wrap: break-word;
+
+  ${({lineLimit=1,lineHeight})=>1===lineLimit?styled_components_browser_esm.AH`
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        `:styled_components_browser_esm.AH`
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: ${lineLimit};
+          /* Fallback for -webkit-line-clamp */
+          max-height: ${lineHeight*lineLimit}px;
+        `}
+`;var ComponentAbstraction=__webpack_require__("./packages/react-sandbox/src/_lib/ComponentAbstraction.tsx"),index_esm=__webpack_require__("./node_modules/.pnpm/@charcoal-ui+utils@4.2.1/node_modules/@charcoal-ui/utils/dist/index.esm.js");const MenuListItemContext=react.createContext({padding:24});function MenuListItem({primary,secondary,onClick,disabled=!1,noHover=!1,gtmClass,children}){const{padding}=(0,react.useContext)(MenuListItemContext);return(0,jsx_runtime.jsxs)(Item,{hasSubLabel:void 0!==secondary,onClick:e=>!disabled&&onClick&&onClick(e),sidePadding:padding,noHover,noClick:void 0===onClick,"aria-disabled":disabled,role:void 0!==onClick?"button":void 0,className:void 0!==gtmClass?`gtm-${gtmClass}`:void 0,children:[(0,jsx_runtime.jsxs)(Labels,{children:[(0,jsx_runtime.jsx)(PrimaryText,{children:(0,jsx_runtime.jsx)(TextEllipsis,{lineHeight:22,lineLimit:1,children:primary})}),void 0!==secondary&&(0,jsx_runtime.jsx)(SecondaryText,{children:(0,jsx_runtime.jsx)(TextEllipsis,{lineHeight:22,lineLimit:1,children:secondary})})]}),children]})}const Item=styled_components_browser_esm.Ay.div`
+  display: flex;
+  height: ${p=>p.hasSubLabel?56:40}px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 ${p=>p.sidePadding}px;
+  user-select: none;
+  cursor: ${p=>p.noClick?"default":"pointer"};
+  transition: 0.2s background-color;
+
+  &:hover {
+    ${p=>!p.noHover&&styled_components_browser_esm.AH`
+        background-color: ${({theme})=>theme.color.surface3};
+      `}
+  }
+
+  ${(0,styled.w)((o=>o.disabled))}
+
+  ${index_esm.gT} {
+    cursor: default;
+    pointer-events: none;
+
+    &:hover {
+      background-color: unset;
+    }
+  }
+`,Labels=styled_components_browser_esm.Ay.div`
+  display: flex;
+  flex-direction: column;
+`,PrimaryText=styled_components_browser_esm.Ay.div`
+  color: ${p=>p.theme.color.text2};
+  line-height: 22px;
+  font-size: 14px;
+  display: grid;
+`,SecondaryText=styled_components_browser_esm.Ay.div`
+  color: ${p=>p.theme.color.text3};
+  line-height: 18px;
+  font-size: 10px;
+`;function MenuListLinkItem({link,onClick,disabled=!1,primary,secondary,gtmClass,noHover,children,...linkProps}){const{Link}=(0,ComponentAbstraction.FY)(),props={disabled,primary,secondary,gtmClass,noHover,children};return disabled?(0,jsx_runtime.jsx)("span",{onClick,children:(0,jsx_runtime.jsx)(MenuListItem,{...props})}):(0,jsx_runtime.jsx)(A,{as:Link,to:link,onClick,...linkProps,children:(0,jsx_runtime.jsx)(MenuListItem,{onClick:()=>{},...props})})}const A=styled_components_browser_esm.Ay.a`
+  display: block;
+`;styled_components_browser_esm.Ay.div`
+  display: grid;
+  gap: 8px;
+  grid-auto-flow: column;
+  align-items: center;
+`,styled_components_browser_esm.Ay.div`
+  color: ${({theme})=>theme.color.text3};
+  display: flex;
+`,styled_components_browser_esm.Ay.div`
+  height: 24px;
+`,styled_components_browser_esm.Ay.div`
+  padding: 0 16px;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${({theme})=>theme.color.text3};
+  margin-top: -2px;
+  margin-bottom: 6px;
+`;function LeftMenu({links,active}){const{Link}=(0,ComponentAbstraction.FY)();return(0,jsx_runtime.jsx)(Container,{children:links.map(((link,index)=>(0,jsx_runtime.jsx)(Link,{to:link.to,children:(0,jsx_runtime.jsx)(LinkItem,{"aria-current":link.id===active||void 0,children:link.text})},index)))})}MenuListItem.__docgenInfo={description:"",methods:[],displayName:"MenuListItem",props:{primary:{required:!0,tsType:{name:"union",raw:"string | React.ReactNode",elements:[{name:"string"},{name:"ReactReactNode",raw:"React.ReactNode"}]},description:""},secondary:{required:!1,tsType:{name:"string"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"(e: React.MouseEvent) => void",signature:{arguments:[{type:{name:"ReactMouseEvent",raw:"React.MouseEvent"},name:"e"}],return:{name:"void"}}},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},gtmClass:{required:!1,tsType:{name:"string"},description:""},noHover:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}},MenuListLinkItem.__docgenInfo={description:"",methods:[],displayName:"MenuListLinkItem",props:{primary:{required:!0,tsType:{name:"union",raw:"string | React.ReactNode",elements:[{name:"string"},{name:"ReactReactNode",raw:"React.ReactNode"}]},description:""},secondary:{required:!1,tsType:{name:"string"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"(e: React.MouseEvent) => void",signature:{arguments:[{type:{name:"ReactMouseEvent",raw:"React.MouseEvent"},name:"e"}],return:{name:"void"}}},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},gtmClass:{required:!1,tsType:{name:"string"},description:""},noHover:{required:!1,tsType:{name:"boolean"},description:""},link:{required:!0,tsType:{name:"string"},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}},composes:["Omit"]};const Container=styled_components_browser_esm.Ay.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`,LinkItem=styled_components_browser_esm.Ay.div`
+  display: flex;
+  align-items: center;
+  color: ${({theme})=>theme.color.text3};
+  border-radius: 24px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 22px;
+  padding: 0 16px;
+  height: 40px;
+  transition: 0.2s color;
+  &:hover {
+    transition: 0.2s color;
+    color: ${({theme})=>theme.color.text2};
+  }
+  &[aria-current] {
+    color: ${({theme})=>theme.color.text2};
+    background-color: ${({theme})=>theme.color.surface3};
+  }
+`;LeftMenu.__docgenInfo={description:"",methods:[],displayName:"LeftMenu",props:{links:{required:!0,tsType:{name:"unknown"},description:""},active:{required:!0,tsType:{name:"ID"},description:""}}};const index_story={title:"react-sandbox/LeftMenu",component:LeftMenu},links=[{text:"text1",to:"",id:"1"},{text:"text2",to:"",id:"2"},{text:"text3",to:"",id:"3"}],Default={render:props=>(0,jsx_runtime.jsx)(LeftMenu,{...props,links,active:"1",children:"children"})};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:'{\n  render: props => {\n    return <LeftMenu {...props} links={links} active="1">\n        children\n      </LeftMenu>;\n  }\n}',...Default.parameters?.docs?.source}}}},"./packages/react-sandbox/src/_lib/ComponentAbstraction.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{FY:()=>useComponentAbstraction});var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),react__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/index.js");const DefaultLink=react__WEBPACK_IMPORTED_MODULE_1__.forwardRef((function DefaultLink({to,children,...rest},ref){return(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a",{href:to,ref,...rest,children})})),DefaultValue={Link:DefaultLink},ComponentAbstractionContext=react__WEBPACK_IMPORTED_MODULE_1__.createContext(DefaultValue);function useComponentAbstraction(){return(0,react__WEBPACK_IMPORTED_MODULE_1__.useContext)(ComponentAbstractionContext)}DefaultLink.__docgenInfo={description:"",methods:[],displayName:"DefaultLink",props:{to:{required:!0,tsType:{name:"string"},description:"リンクのURL"}}}},"./packages/react-sandbox/src/styled.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{w:()=>theme});var styled_components__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/.pnpm/styled-components@5.3.11_@babel+core@7.26.8_react-dom@18.3.1_react@18.3.1__react-is@18.3.1_react@18.3.1/node_modules/styled-components/dist/styled-components.browser.esm.js");const theme=(0,__webpack_require__("./node_modules/.pnpm/@charcoal-ui+styled@4.2.1_react@18.3.1_styled-components@5.3.11_@babel+core@7.26.8_reac_fc4b25586dda67d14437fc98aefc0a14/node_modules/@charcoal-ui/styled/dist/index.esm.js").an)(styled_components__WEBPACK_IMPORTED_MODULE_1__.Ay)}}]);
+//# sourceMappingURL=react-sandbox-src-components-LeftMenu-index-story.b51e7cc4.iframe.bundle.js.map
---
+++ react-sandbox-src-components-Pager-index-story.5a0729f1.iframe.bundle.js
@@ -0,0 +1,76 @@
+"use strict";(self.webpackChunkcharcoal_ui=self.webpackChunkcharcoal_ui||[]).push([[4705],{"./packages/react-sandbox/src/components/Pager/index.story.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,default:()=>index_story});var jsx_runtime=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),react=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),styled_components_browser_esm=__webpack_require__("./node_modules/.pnpm/styled-components@5.3.11_@babel+core@7.26.8_react-dom@18.3.1_react@18.3.1__react-is@18.3.1_react@18.3.1/node_modules/styled-components/dist/styled-components.browser.esm.js");__webpack_require__("./node_modules/.pnpm/warning@4.0.3/node_modules/warning/warning.js");function DotsIcon({size}){return(0,jsx_runtime.jsx)(StyledSVG,{viewBox:"0 0 20 6",width:size,height:size,children:(0,jsx_runtime.jsx)("path",{fillRule:"evenodd",d:"M5,14.5 C3.61928813,14.5 2.5,13.3807119 2.5,12 C2.5,10.6192881 3.61928813,9.5 5,9.5\n          C6.38071187,9.5 7.5,10.6192881 7.5,12 C7.5,13.3807119 6.38071187,14.5 5,14.5 Z M12,14.5\n          C10.6192881,14.5 9.5,13.3807119 9.5,12 C9.5,10.6192881 10.6192881,9.5 12,9.5\n          C13.3807119,9.5 14.5,10.6192881 14.5,12 C14.5,13.3807119 13.3807119,14.5 12,14.5 Z M19,14.5\n          C17.6192881,14.5 16.5,13.3807119 16.5,12 C16.5,10.6192881 17.6192881,9.5 19,9.5\n          C20.3807119,9.5 21.5,10.6192881 21.5,12 C21.5,13.3807119 20.3807119,14.5 19,14.5 Z",transform:"translate(-2 -9)"})})}DotsIcon.defaultProps={size:16};const StyledSVG=styled_components_browser_esm.Ay.svg`
+  fill: currentColor;
+`;DotsIcon.__docgenInfo={description:"",methods:[],displayName:"DotsIcon",props:{size:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:"",defaultValue:{value:"16",computed:!1}},subLink:{required:!1,tsType:{name:"boolean"},description:""}}};var utils=__webpack_require__("./packages/react-sandbox/src/foundation/utils.ts"),WedgeDirection=function(WedgeDirection){return WedgeDirection.Up="up",WedgeDirection.Down="down",WedgeDirection.Left="left",WedgeDirection.Right="right",WedgeDirection}({});function WedgeIcon({size,direction}){return(0,jsx_runtime.jsx)("svg",{viewBox:"0 0 10 8",width:size,height:size,children:(0,jsx_runtime.jsx)(StyledPolyline,{strokeWidth:"2",points:"1,2 5,6 9,2",transform:directionToTransform(direction)})})}function directionToTransform(direction){switch(direction){case"up":return"rotate(180 5 4)";case"down":return;case"left":return"rotate(90 5 4)";case"right":return"rotate(-90 5 4)";default:return(0,utils.H)(direction)}}WedgeIcon.defaultProps={size:16,white:!1,lightGray:!1};const StyledPolyline=styled_components_browser_esm.Ay.polyline`
+  fill: none;
+  stroke-linejoin: round;
+  stroke-linecap: round;
+  stroke: currentColor;
+`;WedgeIcon.__docgenInfo={description:"",methods:[],displayName:"WedgeIcon",props:{size:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:"",defaultValue:{value:"16",computed:!1}},direction:{required:!0,tsType:{name:"WedgeDirection"},description:""},white:{defaultValue:{value:"false",computed:!1},required:!1},lightGray:{defaultValue:{value:"false",computed:!1},required:!1}}};__webpack_require__("./packages/react-sandbox/src/_lib/ComponentAbstraction.tsx");function usePagerWindow(page,pageCount,pageRangeDisplayed=7){const window=(0,react.useMemo)((()=>{const visibleLastPage=Math.min(pageCount,Math.max(page+Math.floor(pageRangeDisplayed/2),pageRangeDisplayed));if(visibleLastPage<=pageRangeDisplayed)return Array.from({length:1+visibleLastPage-1},((_,i)=>1+i));{const start=visibleLastPage-(pageRangeDisplayed-1)+2;return[1,"...",...Array.from({length:1+visibleLastPage-start},((_,i)=>start+i))]}}),[page,pageCount,pageRangeDisplayed]);return(0,react.useDebugValue)(window),window}const Pager=(0,react.memo)((function Pager({page,pageCount,pageRangeDisplayed,onChange}){const window=usePagerWindow(page,pageCount,pageRangeDisplayed),makeClickHandler=(0,react.useCallback)((value=>()=>{onChange(value)}),[onChange]),hasNext=page<pageCount,hasPrev=page>1;return(0,jsx_runtime.jsxs)(PagerContainer,{children:[(0,jsx_runtime.jsx)(CircleButton,{type:"button",hidden:!hasPrev,disabled:!hasPrev,onClick:makeClickHandler(Math.max(1,page-1)),noBackground:!0,children:(0,jsx_runtime.jsx)(WedgeIcon,{size:16,direction:WedgeDirection.Left})}),window.map((p=>"..."===p?(0,jsx_runtime.jsx)(Spacer,{children:(0,jsx_runtime.jsx)(DotsIcon,{size:20})},p):p===page?(0,jsx_runtime.jsx)(CircleButton,{type:"button","aria-current":!0,children:(0,jsx_runtime.jsx)(Text,{children:p})},p):(0,jsx_runtime.jsx)(CircleButton,{type:"button",onClick:makeClickHandler(p),children:(0,jsx_runtime.jsx)(Text,{children:p})},p))),(0,jsx_runtime.jsx)(CircleButton,{type:"button",hidden:!hasNext,disabled:!hasNext,onClick:makeClickHandler(Math.min(pageCount,page+1)),noBackground:!0,children:(0,jsx_runtime.jsx)(WedgeIcon,{size:16,direction:WedgeDirection.Right})})]})}));const PagerContainer=styled_components_browser_esm.Ay.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,CircleButton=styled_components_browser_esm.Ay.button`
+  font-size: 1rem;
+  line-height: calc(1em + 8px);
+  text-decoration: none;
+  border: none;
+  outline: none;
+  touch-action: manipulation;
+  user-select: none;
+  transition: box-shadow 0.2s ease 0s, color 0.2s ease 0s,
+    background 0.2s ease 0s, opacity 0.2s ease 0s;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: content-box;
+  min-width: 24px;
+  min-height: 24px;
+  padding: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  /* HACK:
+   * Safari doesn't correctly repaint the elements when they're reordered in response to interaction.
+   * This forces it to repaint them. This doesn't work if put on the parents either, has to be here.
+   */
+  /* stylelint-disable-next-line property-no-vendor-prefix */
+  -webkit-transform: translateZ(0);
+
+  &[hidden] {
+    visibility: hidden;
+    display: block;
+  }
+
+  border-radius: 48px;
+
+  background: transparent;
+  color: ${({theme})=>theme.color.text3};
+
+  &:hover {
+    background: ${({theme})=>theme.color.surface3};
+    color: ${({theme})=>theme.color.text2};
+  }
+
+  &[aria-current] {
+    background-color: ${({theme})=>theme.color.surface6};
+    color: ${({theme})=>theme.color.text5};
+  }
+
+  &[aria-current]:hover {
+    background-color: ${({theme})=>theme.color.surface6};
+    color: ${({theme})=>theme.color.text5};
+  }
+
+  ${({noBackground=!1})=>noBackground&&styled_components_browser_esm.AH`
+      /* stylelint-disable-next-line no-duplicate-selectors */
+      &:hover {
+        background: transparent;
+      }
+    `}
+`,Spacer=(0,styled_components_browser_esm.Ay)(CircleButton).attrs({type:"button",disabled:!0})`
+  && {
+    color: ${({theme})=>theme.color.text3};
+    background: none;
+  }
+`,Text="span",index_story={title:"react-sandbox/Pager",component:Pager,args:{page:5,pageCount:10}},Default={render:props=>(0,jsx_runtime.jsx)(Pager,{...props,children:"children"})};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  render: props => {\n    return <Pager {...props}>children</Pager>;\n  }\n}",...Default.parameters?.docs?.source}}}},"./packages/react-sandbox/src/_lib/ComponentAbstraction.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{FY:()=>useComponentAbstraction});var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),react__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/.pnpm/react@18.3.1/node_modules/react/index.js");const DefaultLink=react__WEBPACK_IMPORTED_MODULE_1__.forwardRef((function DefaultLink({to,children,...rest},ref){return(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a",{href:to,ref,...rest,children})})),DefaultValue={Link:DefaultLink},ComponentAbstractionContext=react__WEBPACK_IMPORTED_MODULE_1__.createContext(DefaultValue);function useComponentAbstraction(){return(0,react__WEBPACK_IMPORTED_MODULE_1__.useContext)(ComponentAbstractionContext)}DefaultLink.__docgenInfo={description:"",methods:[],displayName:"DefaultLink",props:{to:{required:!0,tsType:{name:"string"},description:"リンクのURL"}}}},"./packages/react-sandbox/src/foundation/utils.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{function unreachable(value){throw new Error(0===arguments.length?"unreachable":`unreachable (${JSON.stringify(value)})`)}__webpack_require__.d(__webpack_exports__,{H:()=>unreachable})}}]);
+//# sourceMappingURL=react-sandbox-src-components-Pager-index-story.5a0729f1.iframe.bundle.js.map
