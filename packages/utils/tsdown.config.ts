76a8598d35cc79d2926fe7fa9df82b458e796e83
yue <yue@pixiv.co.jp>
2025-10-18 20:50:24 +0900

    feat: convert @charcoal-ui/utils to tsdown and esm

--- packages/utils/tsdown.config.ts
+++ packages/utils/tsdown.config.ts
@@ -3,11 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  outExtension({ format }) {
-    return {
-      js: `.${format}.js`,
-    }
-  },
   target: 'esnext',
   sourcemap: true,
+  dts: true
 })
