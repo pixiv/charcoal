5b451de3a55052052da6890315f49c8041041267
mimokmt <103708329+mimokmt@users.noreply.github.com>
2026-02-18 13:35:14 +0900

    refactor(react-sandbox): replace @storybook/react-webpack5 with @storybook/react-vite

--- packages/react-sandbox/src/components/LeftMenu/index.story.tsx
+++ packages/react-sandbox/src/components/LeftMenu/index.story.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react-webpack5'
+import { Meta, StoryObj } from '@storybook/react-vite'
 import LeftMenu from '.'
 
 export default {
--- packages/react-sandbox/src/components/Pager/index.story.tsx
+++ packages/react-sandbox/src/components/Pager/index.story.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react-webpack5'
+import { Meta, StoryObj } from '@storybook/react-vite'
 import Pager from '.'
 
 export default {
