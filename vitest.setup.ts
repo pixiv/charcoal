3f794dfac89be41d17994c3d54b156c391cd141d
napochaan <naporin0624@gmail.com>
2026-07-06 18:43:38 +0900

    test: scrollTo スタブを root の vitest.setup.ts に移動

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -31,6 +31,23 @@ beforeAll(() => {
     CSS.supports = () => false
   }
 
+  // jsdom は Element.prototype.scrollTo を実装していない。
+  if (typeof Element.prototype.scrollTo !== 'function') {
+    Element.prototype.scrollTo = function (
+      this: Element,
+      optionsOrX?: ScrollToOptions | number,
+      y?: number,
+    ) {
+      if (typeof optionsOrX === 'object' && optionsOrX !== null) {
+        if (optionsOrX.left !== undefined) this.scrollLeft = optionsOrX.left
+        if (optionsOrX.top !== undefined) this.scrollTop = optionsOrX.top
+      } else {
+        if (optionsOrX !== undefined) this.scrollLeft = optionsOrX
+        if (y !== undefined) this.scrollTop = y
+      }
+    }
+  }
+
   vi.stubGlobal(
     'matchMedia',
     vi.fn().mockImplementation((query: string) => ({
