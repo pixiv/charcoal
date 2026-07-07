44640d11b585c316b21c2bc0ba31f0178a1119f9
naporitan <naporin0624@gmail.com>
2026-07-07 18:36:17 +0900

    Revert "Revert "fix(react): カルーセルのグラデーションを mask 方式に変更""

--- packages/react/src/components/Carousel/index.tsx
+++ packages/react/src/components/Carousel/index.tsx
@@ -217,13 +217,16 @@ const Carousel = forwardRef<CarouselHandlerRef, CarouselProps>(function Render(
       aria-roledescription="carousel"
       aria-label="Carousel"
     >
-      <div className="charcoal-carousel__viewport">
+      {/* フォーカスリングは viewport に描く（理由は index.css の同セレクタ参照） */}
+      <div
+        className="charcoal-carousel__viewport"
+        data-focus-visible={scrollerFocusVisible || undefined}
+      >
         <div
           {...mergeProps(scrollerFocusProps, keyboardProps)}
           ref={scrollerRef}
           className="charcoal-carousel__scroller"
           tabIndex={0}
-          data-focus-visible={scrollerFocusVisible || undefined}
         >
           {props.items.map((item, i) => (
             <CarouselSlide
